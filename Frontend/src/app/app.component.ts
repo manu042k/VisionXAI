2aad489c607d86e7d6ba7880bc37e52b9b22880f
Manoj <manu042k@gmail.com>
2025-01-12 13:45:55 -0800

    Add Tailwind CSS and PrimeNG integration with initial styles and components

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { SplitterModule } from 'primeng/splitter';
+import { Fluid } from 'primeng/fluid';
+import { Panel } from 'primeng/panel';
+import { Fieldset } from 'primeng/fieldset';
+import { Skeleton } from 'primeng/skeleton';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
+import { AutoFocus } from 'primeng/autofocus';
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    ButtonModule,
+    Menubar,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    Ripple,
+    CommonModule,
+    SplitterModule,
+    Panel,
+    Fluid,
+    Fieldset,
+    Skeleton,
+    FormsModule,
+    TextareaModule,
+    AutoFocus,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+  constructor(private primeng: PrimeNG) {}
+
+  public value: string = '';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [];
+  }
 }
--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -5,11 +5,21 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { MyPreset } from 'src/mytheme-2';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+      },
+    }),
   ],
 };
