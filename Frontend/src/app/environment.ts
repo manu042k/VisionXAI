9f1b881bf5c32c5f816c452e58911794db67928b
Manoj <manu042k@gmail.com>
2025-02-25 16:21:15 -0800

    Update BASE_API_URL in environment.ts for local development and production

--- Frontend/src/app/environment.ts
+++ Frontend/src/app/environment.ts
@@ -5,6 +5,6 @@ export const BASE_API_URL = new InjectionToken<string>('Base API URL');
 
 export function configFactory(): string {
   return isDevMode()
-    ? 'https://annot-a-kezngymp3-manu042kpaperwork-gmailcoms-projects.vercel.app' // Local development
-    : 'https://annot-a-kezngymp3-manu042kpaperwork-gmailcoms-projects.vercel.app'; // Production
+    ? 'https://annot-a-ix-git-production-manu042kpaperwork-gmailcoms-projects.vercel.app' // Local development
+    : 'https://annot-a-ix-git-production-manu042kpaperwork-gmailcoms-projects.vercel.app'; // Production
 }
